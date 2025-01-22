4f70068d1ea19a14148a62a2fe3ceb1538a9f190
Ivan Ignatiev <ignatiev.ivan@gmail.com>
2025-01-22 09:51:35 +0100

    Deploy website - based on 3735f8b7fa9c3f8a1977a628524ffb3393ebfb9e

---
+++ assets/js/acecf23e.ce864327.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkivanignatiev=self.webpackChunkivanignatiev||[]).push([[903],{1912:a=>{a.exports=JSON.parse('{"blogBasePath":"/","blogTitle":"Blog","authorsListPath":"/authors"}')}}]);
