1c62fdc3e135899a83850558b2c84217936f76d1
Ivan Ignatiev <ignatiev.ivan@gmail.com>
2025-01-23 07:35:47 +0100

    Deploy website - based on 989fe1389daaacaa344b0adf13992317b53ab428

--- assets/js/814f3328.8d229622.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkivanignatiev=self.webpackChunkivanignatiev||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Visual Communication Tools for Architecture","permalink":"/visual-communication-tools-for-architecture","unlisted":false,"date":"2025-01-21T09:30:00.000Z"}]}')}}]);
